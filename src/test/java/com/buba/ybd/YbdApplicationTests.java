09887a667a7849b21118fed1b6a5e6bd787536c9
kang231623 <AS231623as>
2020-02-15 21:42:52 +0800

    第一次完整猿宝典项目框架

---
+++ src/test/java/com/buba/ybd/YbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buba.ybd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YbdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
